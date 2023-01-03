6e15a93786aba289b2ecb9a369f939d5ae90db08
katydidya92 <sweets1327@gmail.com>
2023-01-04 08:16:59 +0900

    JPA Auditing 추가

--- src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -4,7 +4,7 @@ import com.example.project.AbstractIntegrationContainerBaseTest
 import com.example.project.pharmacy.entity.Pharmacy
 import org.springframework.beans.factory.annotation.Autowired
 
-import java.lang.reflect.Array
+import java.time.LocalDateTime
 
 class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
@@ -61,4 +61,25 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
         result.size() == 1
 
     }
+
+    def "BaseTimeEntity 등록"() {
+        given:
+        LocalDateTime now = LocalDateTime.now()
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .build()
+
+        when:
+        pharmacyRepository.save(pharmacy)
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getCreatedDate().isAfter(now)
+        result.get(0).getModifiedDate().isAfter(now)
+    }
+
 }

41212383c0a06ff1ea5f6a2486b5a3bcce2e1ee2
katydidya92 <sweets1327@gmail.com>
2023-01-06 22:47:45 +0900

    추천 결과 저장 기능 구현 및 테스트

--- src/main/java/com/example/project/direction/service/DirectionService.java
+++ src/main/java/com/example/project/direction/service/DirectionService.java
@@ -2,10 +2,13 @@ package com.example.project.direction.service;
 
 import com.example.project.api.dto.DocumentDto;
 import com.example.project.direction.entity.Direction;
+import com.example.project.direction.repository.DirectionRepository;
 import com.example.project.pharmacy.service.PharmacySearchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
 import java.util.Collections;
 import java.util.Comparator;
@@ -21,6 +24,15 @@ public class DirectionService {
     private static final int MAX_SEARCH_COUNT = 3;// 약국 최대 검색 갯수
     private static final double RADIUS_KM = 10.0;// 반경 10km
     private final PharmacySearchService pharmacySearchService;
+    private final DirectionRepository directionRepository;
+
+    @Transactional
+    public List<Direction> saveAll(List<Direction> directionList) {
+        if (CollectionUtils.isEmpty(directionList)) {
+            return Collections.emptyList();
+        }
+        return directionRepository.saveAll(directionList);
+    }
 
     public List<Direction> buildDirectionList(DocumentDto documentDto) {
 
--- src/test/groovy/com/example/project/pharmacy/service/PharmacyRepositoryServiceTest.groovy
+++ src/test/groovy/com/example/project/pharmacy/service/PharmacyRepositoryServiceTest.groovy
@@ -1,6 +1,7 @@
 package com.example.project.pharmacy.service
 
 import com.example.project.AbstractIntegrationContainerBaseTest
+import com.example.project.api.service.KakaoAddressSearchService
 import com.example.project.pharmacy.entity.Pharmacy
 import com.example.project.pharmacy.repository.PharmacyRepository
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,6 +13,9 @@ class PharmacyRepositoryServiceTest extends AbstractIntegrationContainerBaseTest
 
     @Autowired
     private PharmacyRepository pharmacyRepository
+
+    @Autowired
+    private KakaoAddressSearchService kakaoAddressSearchService;
 
     def setup() {
         pharmacyRepository.deleteAll()
@@ -106,5 +110,28 @@ class PharmacyRepositoryServiceTest extends AbstractIntegrationContainerBaseTest
         then:
         def result = pharmacyRepositoryService.findAll()
         result.get(0).getPharmacyAddress() == inputAddress
+    }
+
+    def "정상적인 주소를 입력했을 경우, 정상적으로 위도 경도로 변환 된다."() {
+
+        given:
+        boolean actualResult = false
+
+        when:
+        def searchResult = kakaoAddressSearchService.requestAddressSearch(inputAddress)
+
+        then:
+        if(searchResult == null) actualResult = false
+        else actualResult = searchResult.getDocumentDtoList().size() > 0
+
+        where:
+        inputAddress                            | expectedResult
+        "서울 특별시 성북구 종암동"                 | true
+        "서울 성북구 종암동 91"                    | true
+        "서울 대학로"                             | true
+        "서울 성북구 종암동 잘못된 주소"             | false
+        "광진구 구의동 251-45"                    | true
+        "광진구 구의동 251-455555"                | false
+        ""                                      | false
     }
 }
